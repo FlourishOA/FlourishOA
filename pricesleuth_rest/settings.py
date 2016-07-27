afd2b74cceff46215b4d6b658b99d5bc4aa55a32
Patrick Spieker <pspieker@cs.washington.edu>
2016-07-27 12:48:18 -0700

    Updated static files

--- pricesleuth_rest/settings.py
+++ pricesleuth_rest/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = '/home/ubuntu/pricesleuth_rest/static'
+STATIC_ROOT = '/home/ubuntu/pricesleuth_rest/static'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
