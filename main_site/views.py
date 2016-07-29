7ee941d1c72eff25598308d4f389e77d1ec5e8d0
Patrick Spieker <pspieker@cs.washington.edu>
2016-07-29 12:04:16 -0700

    Started writing HTML

--- main_site/views.py
+++ main_site/views.py
@@ -9,4 +9,6 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['most_expensive'] = Price.objects.all().order_by('-price')[:5]
+        context['num_journals'] = Journal.objects.all().count()
+        context['num_prices'] = Price.objects.all().count()
         return context
