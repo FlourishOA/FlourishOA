3c25f558e6b2b030993966d22b9ed48fae2f5547
Patrick Spieker <pspieker@cs.washington.edu>
2016-07-29 11:25:15 -0700

    Fixed static files, still needs some cleanup

--- pricesleuth_rest/urls.py
+++ pricesleuth_rest/urls.py
@@ -22,10 +22,9 @@ from rest_framework.authtoken import views as auth_views
 
 
 urlpatterns = [
-    url(r'^$', include('main_site.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^api-token-auth/', auth_views.ObtainAuthToken.as_view()),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', admin.site.urls),
-
+    url(r'^', include('main_site.urls')),
 ]
