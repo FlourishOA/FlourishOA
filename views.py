8594be7b1b707b4338f5824a0b76bf5833293faa
Patrick Spieker <pspieker@cs.washington.edu>
2016-08-03 03:04:09 -0700

    Updated site with older models

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from api.models import Journal, PriceInfluence
+from api.models import Journal, Price
 
 
 class IndexView(TemplateView):
@@ -8,9 +8,9 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['most_expensive'] = PriceInfluence.objects.all().order_by('-price')[:5]
+        context['most_expensive'] = Price.objects.all().order_by('-price')[:5]
         context['num_journals'] = Journal.objects.all().count()
-        context['num_prices'] = PriceInfluence.objects.all().count()
+        context['num_prices'] = Price.objects.all().count()
         return context
 
 class PriceAIView(TemplateView):
