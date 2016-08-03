90c4802ed411ae3fb98676a499d02adff3128e00
Patrick Spieker <pspieker@cs.washington.edu>
2016-08-03 16:44:19 -0700

    Added vis

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import TemplateView
-from api.models import Journal, Price
+from api.models import Journal, Price, Influence
+import json
 
 
 class IndexView(TemplateView):
@@ -13,6 +15,7 @@ class IndexView(TemplateView):
         context['num_prices'] = Price.objects.all().count()
         return context
 
+
 class PriceAIView(TemplateView):
     template_name = "main_site/viz.html"
 
@@ -21,4 +24,23 @@ class PriceAIView(TemplateView):
         price_ai_map = {}
         for i in Journal.objects.all().filter(article_influence__isnull=False):
            pass
+        return context
+
+
+class VisualizationView(TemplateView):
+    template_name = 'main_site/viz.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(VisualizationView, self).get_context_data(**kwargs)
+        pairs = []
+        for price_obj in Price.objects.all():
+            try:
+                ai = Influence.objects.get(journal__issn=price_obj.journal.issn,
+                                           date_stamp__year=price_obj.date_stamp.year)
+                pairs.append((float(price_obj.price), float(ai.article_influence)))
+
+            except ObjectDoesNotExist:
+                continue
+
+        context['pairs'] = json.dumps(pairs)
         return context
