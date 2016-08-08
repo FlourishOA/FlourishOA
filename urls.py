b54d689380eddb8674adb9c2991b8c1c9c915639
Patrick Spieker <pspieker@cs.washington.edu>
2016-08-08 16:34:26 -0700

    Final fix for url conf

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 urlpatterns =[
     url(r'^viz', views.VisualizationView.as_view(), name='viz'),
     url(r'^priceAI/', views.PriceAIView.as_view(), name='priceAI'),
-    url(r'^', views.IndexView.as_view(), name='index'),
+    url(r'', views.IndexView.as_view(), name='index'),
 ]
