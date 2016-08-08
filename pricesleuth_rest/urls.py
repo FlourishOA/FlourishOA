735c09226f9caee2015547788adedd883a5acf4f
Patrick Spieker <pspieker@cs.washington.edu>
2016-08-08 10:50:23 -0700

    Fixed rendering bug?

--- pricesleuth_rest/urls.py
+++ pricesleuth_rest/urls.py
@@ -1,17 +1,4 @@
 """pricesleuth_rest URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
 from django.conf.urls import include
@@ -19,7 +6,6 @@ from django.contrib import admin
 
 # to avoid confusion with actual views file
 from rest_framework.authtoken import views as auth_views
-
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
