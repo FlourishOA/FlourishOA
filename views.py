f1760b298137a13034fa6e097ee81328af4b4658
Patrick Spieker <pspieker@cs.washington.edu>
2016-08-09 17:22:59 -0700

    Updated views for new client

--- views.py
+++ views.py
@@ -21,7 +21,7 @@ class PriceAIView(TemplateView):
     template_name = "main_site/viz.html"
 
     def get_context_data(self, **kwargs):
-        context = super(TemplateView, self).get_context_data(**kwargs)
+        context = super(PriceAIView, self).get_context_data(**kwargs)
         price_ai_map = {}
         for i in Journal.objects.all().filter(article_influence__isnull=False):
            pass
@@ -34,26 +34,12 @@ class VisualizationView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(VisualizationView, self).get_context_data(**kwargs)
         events = []
-        for price_obj in Price.objects.all():
-            try:
-                ai_list = Influence.objects.filter(journal__issn=price_obj.journal.issn,
-                                                   date_stamp__year=price_obj.date_stamp.year)\
-                                                   .order_by('date_stamp')
-                if ai_list:
-                    event = {}
-                    ai = ai_list[0]
-                    ai_ser = InfluenceSerializer(ai)
-                    price_ser = PriceSerializer(price_obj)
-                    journal_ser = JournalSerializer(ai.journal)
-                    event['ai'] = float(ai_ser.data['article_influence'])
-                    event['ai_date_stamp'] = ai_ser.data['date_stamp']
-                    event['price'] = float(price_ser.data['price'])
-                    event['price_date_stamp'] = price_ser.data['date_stamp']
-                    event.update(journal_ser.data)
-                    events.append(event)
-
-            except ObjectDoesNotExist:
-                continue
+        for price in Price.objects.filter(influence__isnull=False):
+            event = PriceSerializer(price).data
+            event.update(JournalSerializer(price.journal))
+            event['article_influence'] = price.influence.article_influence
+            event['est_article_influence'] = price.influence.est_article_influence
+            events.append(event)
 
         context['events'] = json.dumps(events)
         print len(events)
