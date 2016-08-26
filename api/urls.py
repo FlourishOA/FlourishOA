661a6c1d4653a32dc8c0b3ac8d42f3d27a766ac6
Patrick Spieker <pspieker@cs.washington.edu>
2016-08-26 14:03:04 -0700

    Changed up routing scheme

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,15 @@
 from api import views
 from rest_framework import routers
-from django.conf.urls import url
+from django.conf.urls import url, include
 
-urlpatterns = [
-    url('^docs', views.SchemaView.as_view()),
-]
-router = routers.SimpleRouter()
+
+router = routers.DefaultRouter()
 router.register(r'journals', views.JournalViewSet, 'journal')
 router.register(r'prices', views.PriceViewSet, 'price')
 router.register(r'influence', views.InfluenceViewSet, 'influence')
 
 
-urlpatterns += router.urls
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^docs', views.SchemaView.as_view()),
+]
